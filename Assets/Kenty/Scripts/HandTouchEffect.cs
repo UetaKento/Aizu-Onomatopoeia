016a0cd9a6bee8ce31ce07f1aaaa9f039edf199b
Kenty1003 <69253001+UetaKento@users.noreply.github.com>
2022-09-19 18:33:47 +0900

    プロトタイプ完成

--- Assets/Kenty/Scripts/HandTouchEffect.cs
+++ Assets/Kenty/Scripts/HandTouchEffect.cs
@@ -5,71 +5,55 @@ using UnityEngine.UI;
 
 public class HandTouchEffect : MonoBehaviour
 {
-    private bool isMouseEnter = false;
-    public Canvas canvas;
-    public Text targetText;
-
-    private Vector3 beforeMousePosition;
-    private Vector3 afterMousePosition;
+    //public Canvas overrayCanvas;
+    [SerializeField]
+    private Text targetText;
+    [SerializeField]
+    private string displayWord = "ツルツル";
+    [SerializeField]
+    private int scaleupRatio = 10000;
 
     private float hideThreshold; //マウスが動かなくなったらTime.deltaTimeでこの値が蓄積していき、特定の値より大きくなったらTextを隠す。
 
     // Start is called before the first frame update
     void Start()
     {
-        beforeMousePosition = Input.mousePosition;
+        if (scaleupRatio < 0 || targetText == null)
+        {
+            Debug.Log("初期化失敗");
+            return;
+        }
+        targetText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        //afterMousePosition = Input.mousePosition;
-        //if (isMouseEnter)
-        //{
-        //    if (beforeMousePosition != afterMousePosition)
-        //    {
-        //        DisplayText("見える化！");
-        //        Debug.Log("beforeMousePosition " + beforeMousePosition + " / " + "afterMousePosition " + afterMousePosition);
-        //    }
-        //    else
-        //    {
-        //        DisplayText(" ");
-        //    }
-        //}
-        //else
-        //{
 
-        //}
-        //beforeMousePosition = afterMousePosition;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Hit!!!");
-        foreach (ContactPoint point in collision.contacts)
-        {
-            //衝突位置
-            Debug.Log(point);
-        }
     }
 
     private void OnCollisionStay(Collision collision)
     {
+        foreach (ContactPoint point in collision.contacts)
+        {
+            Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(
+                null,
+                point.point
+                );
 
+            //ハンドトラッキングで手が動いている座標は非常に小さな値なので、screenPosも小さな値になってしまう。なので * scaleupRatioをする必要あり。
+            targetText.GetComponent<RectTransform>().anchoredPosition = new Vector2(
+            screenPos.x * scaleupRatio,
+            screenPos.y * scaleupRatio);
+
+            targetText.text = displayWord;
+
+        }
     }
 
-    private void DisplayText(string inputText)
-    {
-        Vector2 MousePos = Vector2.zero;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            canvas.GetComponent<RectTransform>(),
-            Input.mousePosition,
-            canvas.worldCamera,
-            out MousePos);
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    targetText.text = "";
+    //}
 
-        targetText.GetComponent<RectTransform>().anchoredPosition = new Vector2(
-            MousePos.x,
-            MousePos.y);
-        targetText.text = inputText;
-    }
 }
